50dab3044741a514aec20774d0610475b318851b
Trực Trần Trung <tructtpk03625@gmail.com>
2025-01-18 22:22:14 +0700

    feature: Update statistics (Dashboard) page interface (WIP)
    - Redesign the layout and style of the statistics page to improve user experience.
    - Add new charts and graphs to visually represent data.
    - Ensure responsive design for better usability across different devices.
    
    Note: The functionality is still a work in progress and not yet complete.

---
+++ MVCBanXeDap/ViewModels/TopSellingProductsVM.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCBanXeDap.ViewModels
+{
+    public class TopSellingProductsVM
+    {
+        public List<ChiTietHoaDonVM> TopProducts { get; set; }
+        public List<ChiTietHoaDonVM> TopColors { get; set; }
+        public List<ChiTietHoaDonVM> TopSizes { get; set; }
+    }
+}
