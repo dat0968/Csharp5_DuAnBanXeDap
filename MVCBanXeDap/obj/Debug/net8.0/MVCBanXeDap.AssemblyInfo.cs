4e7d1802bf4ce88dedaab6526cfa004a887c64d3
NguyenThanhDat <thanhdat0968367@gmail.com>
2025-01-24 08:18:29 +0700

    delete bin/obj

--- MVCBanXeDap/obj/Debug/net8.0/MVCBanXeDap.AssemblyInfo.cs
+++ MVCBanXeDap/obj/Debug/net8.0/MVCBanXeDap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99de420b9eb46cb656fafc45458a7abd71ad2030")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06b1ffb31d1b000b1259e446b34e615757600551")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
