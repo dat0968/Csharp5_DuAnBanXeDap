2deb1bc9ada0917d16b9e25532f22863b32d1bfc
NguyenThanhDat <thanhdat0968367@gmail.com>
2025-02-25 23:44:35 +0700

    đ

--- MVCBanXeDap/obj/Debug/net8.0/MVCBanXeDap.AssemblyInfo.cs
+++ MVCBanXeDap/obj/Debug/net8.0/MVCBanXeDap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67643ea259ac1678c9b22ee8e9451f26872879c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838d61942b75c73391cd1094f574b29d5e7cd497")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
