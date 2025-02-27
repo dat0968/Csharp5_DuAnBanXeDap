0d5d6a4af02bba1c475747ce599630c97ce15cc6
NguyenThanhDat <thanhdat0968367@gmail.com>
2025-02-27 11:43:52 +0700

    refactor: edit status order

--- APIBanXeDap/obj/Debug/net8.0/APIBanXeDap.AssemblyInfo.cs
+++ APIBanXeDap/obj/Debug/net8.0/APIBanXeDap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIBanXeDap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178c58b52ef15717bc3370c816b3448cc33b12b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8879dd1f61bcc3bd234aa2874370f36c8e88168")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIBanXeDap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIBanXeDap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MVCBanXeDap/obj/Debug/net8.0/MVCBanXeDap.AssemblyInfo.cs
+++ MVCBanXeDap/obj/Debug/net8.0/MVCBanXeDap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178c58b52ef15717bc3370c816b3448cc33b12b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8879dd1f61bcc3bd234aa2874370f36c8e88168")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCBanXeDap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
