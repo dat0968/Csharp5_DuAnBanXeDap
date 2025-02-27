884f5a55e3d1152f9e1fe97d2ef678339ca1fd94
NguyenThanhDat <thanhdat0968367@gmail.com>
2025-02-27 15:05:43 +0700

    d

--- APIBanXeDap/obj/Debug/net8.0/APIBanXeDap.AssemblyInfo.cs
+++ APIBanXeDap/obj/Debug/net8.0/APIBanXeDap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIBanXeDap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8879dd1f61bcc3bd234aa2874370f36c8e88168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+185e2df22c87eb7e12c4999289c3ba03f6a38d9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIBanXeDap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIBanXeDap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
